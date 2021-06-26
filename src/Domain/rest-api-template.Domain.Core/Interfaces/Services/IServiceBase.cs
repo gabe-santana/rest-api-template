dd4597bc081a0989a0c01fa440ef51d4a2f02a2a
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-06-26 17:27:53 -0300

    IServiceBase added

---
+++ src/Domain/rest-api-template.Domain.Core/Interfaces/Services/IServiceBase.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace rest_api_template.Domain.Core.Interfaces.Services
+{
+    public interface IServiceBase<TEntity> where TEntity : class
+    {
+         TEntity Get(int id);
+         IEnumerable<TEntity> GetAll();
+         void Add(TEntity entity);
+         void Update(TEntity entity);
+         void Delete(TEntity entity);
+    }
+}
