b2690ca6afe5ade943cd20caa02b70eededa431e
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-06-26 15:43:12 -0300

    Product entity added to Domain entities

---
+++ src/Domain/rest-api-template.Domain/Entities/Product.cs
@@ -0,0 +1,9 @@
+namespace rest_api_template.Domain.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
