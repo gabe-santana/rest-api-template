553ae82a8fd46c2176e0e80e5243f2294e72132e
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-06-26 17:59:43 -0300

    CustomerDTO and ProductDTO created to Application

---
+++ src/Application/rest-api-template.Application/DTO/ProductDTO.cs
@@ -0,0 +1,9 @@
+namespace rest_api_template.Application.DTO
+{
+    public class ProductDTO
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
