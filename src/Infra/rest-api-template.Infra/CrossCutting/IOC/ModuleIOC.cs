03b02511b94b61dd45e483220443ff39bcd9dcbb
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-06-28 13:01:40 -0300

    CrossCutting/IOC/ModuleIOC created

--- src/Infra/rest-api-template.Infra/CrossCutting/IOC/ModuleIOC.cs
+++ src/Infra/rest-api-template.Infra/CrossCutting/IOC/ModuleIOC.cs
@@ -1,7 +1,12 @@
+using Autofac;
+
 namespace rest_api_template.Infra.CrossCutting.IOC
 {
-    public class ModuleIOC
+    public class ModuleIOC : Module
     {
-        
+        protected override void Load(ContainerBuilder builder)
+        {
+           ConfigurationIOC.Load(builder);
+        }
     }
 }
